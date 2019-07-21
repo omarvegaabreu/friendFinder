a410332bd07c32c839b625badc7cc4082ce63bb9
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-21 17:24:22 -0400

    updated media

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,42 @@
 var friends = [
   {
-    name: "Tom Hardy",
+    name: "Rickson Gracie",
     photo:
-      "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+      "https://www.egjjf.com/wp-content/uploads/2018/10/Alex-Pannain-0075_2015_RICKSON_JJGF_2015IMG_4672201201.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
   },
   {
-    name: "Jamie Dornan",
+    name: "Roberto Cyborg Abreu",
     photo:
-      "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
+      "https://www.jiujitsutimes.com/wp-content/uploads/cyborg-4-768x467.jpg",
     scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
   },
   {
-    name: "Anthony Joshua",
-    photo:
-      "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+    name: "Lucas Leite",
+    photo: "https://i.ytimg.com/vi/WSzMPSrnBx4/hqdefault.jpg",
     scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
   },
   {
-    name: "Henry Cavill",
+    name: "Marcelo Garcia",
     photo:
-      "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/5/5c/Marcelo_garcia.jpg",
     scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
   },
   {
-    name: "Emily Ratajowski",
-    photo:
-      "https://pmchollywoodlife.files.wordpress.com/2014/10/Emily-Ratajkowski-Reveals-Rock-Hard-Abs-In-Teeny-Crop-Top-While-Vacationing-With-Husband-gallery.jpg",
+    name: "Marcos Almeida",
+    photo: "https://akathailand.com/aka-media/2017/06/MBA2.jpg",
     scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
   },
   {
-    name: "Blake Lively",
+    name: "Andre Galvao",
     photo:
-      "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
+      "https://www.graciemag.com/wp-content/uploads/2013/12/BraulioGalvaoSuperfight-62-1.jpg",
     scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
   },
   {
-    name: "Kate Upton",
+    name: "Gordon Ryan",
     photo:
-      "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+      "https://www.bjjheroes.com/wp-content/uploads/2016/04/GordonRyan.jpg",
     scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
   }
 ];
