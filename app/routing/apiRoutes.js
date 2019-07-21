b52a8f062fe822fbf522b6e56f2f7590bbe0d5b7
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-21 12:01:27 -0400

    completed apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,24 @@
-var friends = require("../data/friends");
+const friends = require("../data/friends");
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+module.exports = app => {
+  app.get("/api/friends", (req, res) => {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
-    var totalDifference = 0;
+  app.post("/api/friends", (req, res) => {
+    const totalDifference = 0;
 
-    var bestMatch = {
+    const bestMatch = {
       name: "",
       photo: "",
       friendDifference: 1000
     };
 
-    var userData = req.body;
-    var userName = userData.name;
-    var userScores = userData.scores;
+    const userData = req.body;
+    const userName = userData.name;
+    let userScores = userData.scores;
 
-    var b = userScores.map(function(item) {
+    const b = userScores.map(item => {
       return parseInt(item, 10);
     });
     userData = {
@@ -30,19 +30,19 @@ module.exports = function(app) {
     console.log("Name: " + userName);
     console.log("User Score " + userScores);
 
-    var sum = b.reduce((a, b) => a + b, 0);
+    const sum = b.reduce((a, b) => a + b, 0);
 
     console.log("Sum of users score " + sum);
     console.log("Best match friend diff " + bestMatch.friendDifference);
     console.log("+++++++=================++++++++++");
 
-    for (var i = 0; i < friends.length; i++) {
+    for (let i = 0; i < friends.length; i++) {
       console.log(friends[i].name);
       totalDifference = 0;
       console.log("Total Diff " + totalDifference);
       console.log("Best match friend diff " + bestMatch.friendDifference);
 
-      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      const bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
       console.log("Total friend score " + bfriendScore);
       totalDifference += Math.abs(sum - bfriendScore);
       console.log("-------------------------> " + totalDifference);
