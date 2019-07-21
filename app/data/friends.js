88177779329894449dc4b541d842506d23a58092
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-21 10:13:25 -0400

    completed json array and set up routes

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+const friends = [
+  {
+    name: `Marcos Almeida`,
+    photo:
+      "https://www.egjjf.com/wp-content/uploads/2018/10/Alex-Pannain-0075_2015_RICKSON_JJGF_2015IMG_4672201201.jpg",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: `Roberto "Cyborg" Abreu`,
+    photo:
+      "https://cdn.shopify.com/s/files/1/1800/2299/files/77d0b07dc6debd3f525bf946c2a9a960_large.jpg?v=1544303172",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: `Marcelo Garcia`,
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/5/5c/Marcelo_garcia.jpg",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: `Renzo Gracie`,
+    photo:
+      "https://static1.squarespace.com/static/5a57d5e1268b962ed07665bb/t/5a59778ae4966bd4a50b5cc3/1516673336292/renzo_portrait+%281%29.jpg?format=1000w",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: `Lucas Leite`,
+    photo:
+      "https://www.bjjheroes.com/wp-content/uploads/2014/07/Lucas-Leite.jpg",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: `Joao Gabriel Rocha`,
+    photo:
+      "https://www.bjjheroes.com/wp-content/uploads/2014/07/Joao-Gabriel-Rocha.jpg",
+    score: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+/***Dependencies */
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+/**Express middleware */
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port's`, PORT));
+/**Body parser middleware */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+/**Routes */
+app.use(express.static("app/public"));
+
+/**Require routes */
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+/***If correct will log /listening on port 3000 */
+app.listen(PORT, () => console.log(`Listening on PORT, ${PORT}`));
